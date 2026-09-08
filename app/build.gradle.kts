e8a75e5acb7f74039e94910b0f698190c5501a95
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-08 21:08:54 +0200

    Add on-device OCR dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     implementation("androidx.camera:camera-lifecycle:1.4.1")
     implementation("androidx.camera:camera-view:1.4.1")
     implementation("androidx.exifinterface:exifinterface:1.3.7")
+    implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.1")
 }
