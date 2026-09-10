acdb02f9c81530dedbec0dc0dd9816db75dbc6bb
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-11 01:55:20 +0200

    Fix PDF viewer Toast import

--- app/src/main/java/com/baltas80/pocketscan/PdfViewerActivity.kt
+++ app/src/main/java/com/baltas80/pocketscan/PdfViewerActivity.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.CreateDocument
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
