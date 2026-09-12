9aeb7ba627c883495cd89bcdaf6a35c30f837666
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-12 13:08:38 +0200

    Prepare PocketScan 1.0.0 release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.baltas80.pocketscan"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "1.0.0"
     }
 
     buildFeatures {
