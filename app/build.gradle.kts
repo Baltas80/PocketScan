2c3cca1cfbe655136fe2020fb11b803e487c2ee5
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-18 14:26:08 +0200

    chore: update AdMob SDK to current release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-mlkit-text-recognition-japanese:16.0.1")
     implementation("com.google.android.gms:play-services-mlkit-text-recognition-korean:16.0.1")
     implementation("com.google.android.gms:play-services-mlkit-document-scanner:16.0.0")
-    implementation("com.google.android.gms:play-services-ads:24.0.0")
+    implementation("com.google.android.gms:play-services-ads:25.5.0")
     implementation("com.google.android.ump:user-messaging-platform:3.2.0")
     implementation("com.google.guava:guava:33.2.1-android")
 
